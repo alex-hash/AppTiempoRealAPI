189d941cf49645229dac8c89da2ebaf83be1bcb0
jesus.changbuccheri <jesus.changbuccheri@PLX300000000664>
2020-05-18 15:24:24 +0200

    Add models

---
+++ RealTimeAPI/API/Models/Apuesta.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models {
+    public class Apuesta {
+        private int idApuesta;
+        private int idEvento;
+        private int idJugador;
+        private decimal cantidad;
+        private DateTime fecha;
+        private string prediccion;
+        private bool? resultado;
+
+        public int IdApuesta { get => idApuesta;  }
+        public int IdEvento { get => idEvento; set => idEvento = value; }
+        public int IdJugador { get => idJugador; set => idJugador = value; }
+        public decimal Cantidad { get => cantidad; set => cantidad = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+        public string Prediccion { get => prediccion; set => prediccion = value; }
+        public bool? Resultado { get => resultado; set => resultado = value; }
+
+        public Apuesta(int idApuesta, int idEvento, int idJugador, decimal cantidad, DateTime fecha, string prediccion, bool resultado) {
+            this.idApuesta = idApuesta;
+            this.idEvento = idEvento;
+            this.idJugador = idJugador;
+            this.cantidad = cantidad;
+            this.fecha = fecha;
+            this.prediccion = prediccion;
+            this.resultado = resultado;
+        }
+
+        public Apuesta(DAL.Apuesta model) {
+            this.idApuesta = model.idApuesta;
+            this.idEvento = model.idEvento;
+            this.idJugador = model.idJugador;
+            this.cantidad = model.cantidad;
+            this.fecha = model.fecha;
+            this.prediccion = model.prediccion;
+            this.resultado = model.resultado;
+        }
+    }
+}
---
+++ RealTimeAPI/API/Models/Monedero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models {
+    public class Monedero {
+        private int idMonedero;
+        private int idJugador;
+        private decimal saldo;
+
+        public int IdMonedero { get => idMonedero; }
+        public int IdJugador { get => idJugador; set => idJugador = value; }
+        public decimal Saldo { get => saldo; set => saldo = value; }
+
+        public Monedero(int idMonedero, int idJugador, decimal saldo) {
+            this.idMonedero = idMonedero;
+            this.idJugador = idJugador;
+            this.saldo = saldo;
+        }
+
+        public Monedero(DAL.Monedero model) {
+            this.idMonedero = model.idMonedero;
+            this.idJugador = model.idJugador;
+            this.saldo = model.saldo;
+        }
+    }
+}
