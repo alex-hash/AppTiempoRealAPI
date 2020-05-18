495284225fe34be0a75319859a98e67753463971
jesus.changbuccheri <jesus.changbuccheri@PLX300000000664>
2020-05-18 16:52:41 +0200

    Add Repository files and ModelExtensions file

---
+++ RealTimeAPI/API/Models/ModelExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models {
+    public static class ModelExtensions {
+        //Extensiones de listas:
+        public static List<Evento> ToLogical(this List<DAL.Evento> model) {
+            //Extension: convierte lista de eventos entity a eventos logicos
+            List<Evento> eventos = new List<Evento>();
+            foreach (DAL.Evento e in model)
+            {
+                eventos.Add(new Evento(e));
+            }
+            return eventos;
+        }
+
+        //public static DAL.Evento ToModel(this Evento logical) {
+        //    //Extension: convirte evento logico a modelo de entity
+        //    DAL.Evento evento = new DAL.Evento();
+        //    evento.idEvento = logical.Id;
+        //    evento.nombre = logical.Nombre;
+        //    evento.fecha = logical.Fecha;
+        //    evento.cuota = (float)logical.Cuota;
+        //    evento.resultado = logical.Resultado;
+        //    return evento;
+        //}
+
+    }
+}
---
+++ RealTimeAPI/API/Models/Repository/EventoRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace API.Models {
+    public class EventoRepository {
+        public List<Evento> ObtenerEventos() {
+            //obtiene todos los eventos
+            List<DAL.Evento> eventos = Handler.DB.Evento.ToList();
+            return eventos.ToLogical();
+        }
+
+        public Evento ObtenerEvento(int idEvento) {
+            //Obtiene un evento determinado dado su id
+            DAL.Evento evento = Handler.DB.Evento.Where(e => e.idEvento == idEvento).FirstOrDefault();
+            if (evento != null)
+            {
+                return new Evento(evento);
+            } else
+            {
+                return null;
+            }
+
+        }
+
+        public bool ModificarCuotaEvento(int idEvento, float nuevaCuota) {
+            //Modifica la cuota de un evento
+            DAL.Evento evento = Handler.DB.Evento.Where(e => e.idEvento == idEvento).FirstOrDefault();
+            if (evento != null)
+            {
+                evento.cuota = nuevaCuota;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
