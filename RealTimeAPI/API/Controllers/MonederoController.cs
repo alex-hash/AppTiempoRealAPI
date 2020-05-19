233adad6850f339aa527828f81cda8cde9265f73
jesus.changbuccheri <jesus.changbuccheri@PLX300000000664>
2020-05-19 13:17:24 +0200

    Add saldo parameter to Monedero POST method and constructor

--- RealTimeAPI/API/Controllers/MonederoController.cs
+++ RealTimeAPI/API/Controllers/MonederoController.cs
@@ -16,6 +16,20 @@ namespace API.Controllers
             return MonederoRespository.NuevoMonedero(id);
         }
 
+        public Monedero Post([FromBody]dynamic model) {
+            //id = idJugador
+            try
+            {
+                int idJugador = Int32.Parse( model["idJugador"] );
+                decimal saldo = decimal.Parse( model["saldo"] );
+                return MonederoRespository.NuevoMonedero(idJugador, saldo);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
         public Monedero Get(int id) {
             //id = idJugador
             return MonederoRespository.ObtenerMonedero(id);
